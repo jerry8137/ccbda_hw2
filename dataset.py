e100e0c50f81a2bedb250179b4ce2d9d3a8bd50e
Jerry <jerry8137@gmail.com>
2022-10-30 14:54:47 +0800

    set mode

--- dataset.py
+++ dataset.py
@@ -11,12 +11,17 @@ class brainDataset(Dataset):
     def __init__(self, root, mode):
         self.root = root
         self.mode = mode
+        assert self.mode=='test' or self.mode=='unlabeled'
+        self.root = os.path.join(self.root, self.mode)
+        self.files = []
         self.parse_file()
 
     def parse_file(self):
         for root, dirs, files in sorted(os.walk(self.root)):
             for name in sorted(files):
-                print(os.path.join(root, name))
+                file_path = (os.path.join(root, name))
+                self.files.append(file_path)
+        print(len(self.files))
 
 def test():
     dataset = brainDataset(root='./data', mode='unlabeled')
