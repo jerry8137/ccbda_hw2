16d350c7f21a29a4f2e31c0b75aeac1b2aa14fb7
Jerry <jerry8137@gmail.com>
2022-11-02 15:32:08 +0800

    add projection head

--- resnet_simclr.py
+++ resnet_simclr.py
@@ -2,14 +2,19 @@ import torch.nn as nn
 import torchvision.models as models
 
 class ResNetSimCLR(nn.Module):
-    def __init__(self, out_dim):
+    def __init__(self, hidden_dim, out_dim):
         super(ResNetSimCLR, self).__init__()
-        self.resnet = models.resnet18(pretrained=False, num_classes=out_dim)
+        self.resnet = models.resnet18(pretrained=False)
+        self.resnet.fc = nn.Identity()
+
         # add projection head
-        self.resnet.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp),
-                                       nn.ReLU(),
-                                       self.resnet.fc)
+        self.projection = nn.Sequential(nn.Linear(512, hidden_dim),
+                                        nn.BatchNorm1d(hidden_dim),
+                                        nn.ReLU(),
+                                        nn.Linear(hidden_dim, out_dim))
 
     def forward(self, x):
-        return self.resnet(x)
+        feature = self.resnet(x)
+        out = self.projection(feature)
+        return out
 
